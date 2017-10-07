f240c6b28cda01c9496fd5e0b02eedd8fbd22b44
Abdelrahman Ahmed <abdelm.is@gmail.com>
2017-10-07 17:36:54 +1100

    Update webapp title and add description

--- webapp/config.py
+++ webapp/config.py
@@ -1,8 +1,8 @@
 class Config(object):
-    SITE_NAME = 'Sentiment Analysis for Movie Reviews'
+    SITE_NAME = 'Sentiment Analysis on Movie Reviews'
     SITE_SLUG_NAME = 'imdb-sentiment-analysis'
     TAGLINES = ['Sentiment Analysis using Deep Learning']
-    SITE_DESCRIPTION = 'Sentiment analysis for movie reviews'
+    SITE_DESCRIPTION = 'Sentiment analysis on movie reviews'
     SITE_KEYWORDS = 'sentiment analysis, imdb, movie, review, nlp, deep learning'
     ADMINS = ['abdelm.is@gmail.com']
 
